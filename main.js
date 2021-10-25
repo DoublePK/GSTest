6270d1639ddf650f1d776d100afc585a45cc19c4
DoublePK <69709703+DoublePK@users.noreply.github.com>
2021-10-25 21:44:22 +0630

    add addGS function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function testGS(){
 
-  const url = "https://script.google.com/macros/s/AKfycbx1thxh7jFTKREi-4acPVqGnoS5JTK43_WDAvwSTS3zg_T3gyzB-NLFOe949UGQMfy-/exec";
+  const url = "https://script.google.com/macros/s/AKfycbwn9NW1BxllCNOYnU1r4wQJ4nO0q-lV4qAIv7wERJyjBV-H6ZjSZc_u7xy4T_UHtoNQ/exec";
 
   fetch(url)
   .then(d => d.json())
@@ -10,3 +10,23 @@ function testGS(){
 }
 
 document.getElementById('btn').addEventListener('click', testGS);
+
+
+
+function addGS(){
+
+  const url = "https://script.google.com/macros/s/AKfycbwn9NW1BxllCNOYnU1r4wQJ4nO0q-lV4qAIv7wERJyjBV-H6ZjSZc_u7xy4T_UHtoNQ/exec";
+
+  fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'no-cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow', 
+    body: JSON.stringify({name:'John'}) // body data type must match "Content-Type" header
+  })
+}
+
+document.getElementById('btn2').addEventListener('click', addGS);
